6483bd2a90759ebd8a716dc1c403835d49f72775
olivarad <olivia.radecki02@gmail.com>
2024-01-29 11:19:45 -0500

    Created files for resources and initial code

---
+++ Source/MP3-Interpreter.cc
@@ -0,0 +1,11 @@
+/**
+ * @file MP3-Interpreter.cc
+ * @author Olivia Radecki (olivia@oliviaradecki.com)
+ * @brief MP3-Interpreter to turn an MP3 file into properly timed button presses on a Harmonix style guitar controller.
+ * @version 0.1
+ * @date 2024-01-29
+ */
+
+int main(){
+    return 0;
+}
